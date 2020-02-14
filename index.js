3c5ee96165c4a0a67264426cb4018d94fad5dd12
Lisa Bamberg <lisambamberg@gmail.com>
2020-02-14 14:59:40 -0600

    Added Select To Copy Entire URL for User

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ document.querySelector("form").addEventListener("submit", event => {
   const encrypted = btoa(input.value);
   //will convert input to base64 string
 
-  document.querySelector(
-    "#link-input"
-  ).value = `${window.location}#${encrypted}`;
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
   //will take encryted value and place it in the link-input
+
+  linkInput.select();
+  //will select entire length of link for user
 });
