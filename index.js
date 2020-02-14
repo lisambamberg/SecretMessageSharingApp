0fe8b847a0a913e5ad435f09cb31380c3a34b5cc
Lisa Bamberg <lisambamberg@gmail.com>
2020-02-14 14:10:21 -0600

    Submitted String Will Become Encrypted

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-document.querySelector('form').addEventListener('submit', event => {
-event.preventDefault();
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
 
-const input = document.querySelector('input');
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  //will convert input to base64 string
 
-console.log(input.value);
-})
+  document.querySelector("#link-input").value = encrypted;
+  //will take encryted value and place it in the link-input
+});
